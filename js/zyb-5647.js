9871468a761c430c5748b98df4614f88ae216f4a
LanaAuada <rm551143@fiap.com.br>
2023-04-18 10:46:34 -0300

    convertendo htmlcollection em array de forma indireta

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,5 +1,5 @@
 // getElementById()
-const liElement = document.getElementsByTagName("li")
+
 
 // for (let index = 0; index < liElement.length; index++) {
 //     console.log(liElement[index].textContent)   
@@ -10,22 +10,30 @@ const liElement = document.getElementsByTagName("li")
 //declaração de arrays
 let nr1 = [1,2,3,4,5];
 let nr2 = [6,7,8,9,10];
-//let nr3 = nr1+nr2;
-let nr3 = [nr1,nr2];
+//espalhar array com spread [...]
+let nr3 = [...nr1,...nr2];
 
 console.log(`Array-1 ${nr1}`);
 console.log(`Array-2 ${nr2}`);
 console.log(`Array-3 ${nr3}`);
 
-//listand um array tecnica especial
+// //listand um array tecnica especial
 
-// nr3.forEach((itemDoArray)=>{
-//     console.log(`Array-3 sendo listado ${itemDoArray}`);
-// })
-//listando array com map
+// // nr3.forEach((itemDoArray)=>{
+// //     console.log(`Array-3 sendo listado ${itemDoArray}`);
+// // })
+// //listando array com map
 nr3.map((numero, key)=>{
     console.log(`${(key+1)} - itens do array - ${numero}`);
-    numero.forEach((digitos)=>{
-        console.log(digitos)
-    })
+
+})
+
+//convertendo htmlcollection em array de forma indireta
+const liElementHtml = document.getElementsByTagName("li");
+const liElementArray = [...liElementHtml];
+
+liElementArray.map((item,key)=>{
+
+    console.log(`${key+1} - ${item.textContent}`)
+
 })
