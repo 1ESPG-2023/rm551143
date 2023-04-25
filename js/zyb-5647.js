0dc1b5c5832b1da05b4e36c7f4780e4a8443a603
LanaAuada <rm551143@fiap.com.br>
2023-04-25 11:10:25 -0300

    random, round, setTimeout

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -39,24 +39,50 @@
     
 // })
 
-function soma(p1,p2,p3){
-    nr1 = p1;
-    nr2 = p2;
+// function soma(p1,p2,p3){
+//     nr1 = p1;
+//     nr2 = p2;
 
-    return (parseInt(nr1)+ parseInt(nr2)+ parseInt(p3))
+//     return (parseInt(nr1)+ parseInt(nr2)+ parseInt(p3))
 
-    // console.log(`Resultado:${parseInt(nr1)+ parseInt(nr2)+ parseInt(parametro)}`);
-    // console.log(`Este é o parâmetro que foi passado para a função:${parametro}`);
+//     // console.log(`Resultado:${parseInt(nr1)+ parseInt(nr2)+ parseInt(parametro)}`);
+//     // console.log(`Este é o parâmetro que foi passado para a função:${parametro}`);
 
-}
+// }
 
-//ARROW FUNCTION
-const mudaCor = ()=>{
+// //ARROW FUNCTION
+// const mudaCor = ()=>{
 
-    //função set-time-out
-    setTimeout(alert("executou"),5000)
+//     //função set-time-out
+//     setTimeout(alert("executou"),5000)
+
+// }
+
+// mudaCor();
+// soma();
+
+let tmp = "";
+
+function mudaCor() {
+
+    let r = "";
+    let g = "";
+    let b = "";
+
+    //random = gera números aleatórios entre 0 e 1
+    //ceil, floor e round que arredondam o número para cima, para baixo ou aleatoriamente
+
+    //determinando para a variável r um valor entre 0 e 255
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+
+    cabecalho.setAttribute("style",`background-color:rgb(${r},${g},${b})`)
+
+    tmp = setTimeout(mudaCor, 500);
 
 }
 
 mudaCor();
-soma();
